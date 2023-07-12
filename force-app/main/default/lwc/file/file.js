1b6940c3f1db1e7dffe76b2ecfa049f35797b007
mahesh200997 <125857514+mahesh200997@users.noreply.github.com>
2023-07-12 12:40:02 +0530

    first commit

---
+++ force-app/main/default/lwc/file/file.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class SubtractButton extends LightningElement {
+  handleClick() {
+    // Handle button click logic here
+    console.log('Button clicked');
+  }
+}
